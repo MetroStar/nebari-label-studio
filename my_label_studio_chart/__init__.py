ad61e79547433439d762ffe39648fdae77286c67
iameskild <eskild.eriksen122@gmail.com>
2023-07-18 02:29:26 -0700

    Add overrides and set domain appropriately

--- my_label_studio_chart/__init__.py
+++ my_label_studio_chart/__init__.py
@@ -1,25 +1,25 @@
 from typing import List
 
 from nebari.hookspecs import NebariStage, hookimpl
-
-from nebari_helm_stage import InputSchema, NebariHelmStage, helm
+from nebari_helm_stage import NebariHelmStage, helm
 
 label_studio = helm.Chart(
     name="label-studio",
     repo="heartex",
     url="https://charts.heartex.com/",
     version="1.1.4",
-    overrides={},
 )
+
+set_json_template = {
+    "label-studio.global.extraEnvironmentVars.LABEL_STUDIO_HOST": "{domain}/label-studio",
+    "ingress.host": "{domain}",
+}
 
 
 class LabelStudioHelmStage(NebariHelmStage):
     name = "my-label-studio"
-
-    input_schema = InputSchema
-    output_schema = None
-
     base_dependency_charts = [label_studio]
+    set_json_template = set_json_template
 
 
 @hookimpl
