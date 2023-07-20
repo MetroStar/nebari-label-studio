63dc41e0373c069b453906e30ca5768aa319a136
iameskild <eskild.eriksen122@gmail.com>
2023-07-20 10:45:12 -0700

    Add required_inputs method

--- my_label_studio_chart/__init__.py
+++ my_label_studio_chart/__init__.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Any, Dict, List
 
 from nebari.hookspecs import NebariStage, hookimpl
 from nebari_helm_stage import NebariHelmStage, helm
@@ -10,16 +10,22 @@ label_studio = helm.Chart(
     version="1.1.4",
 )
 
-set_json_template = {
-    "label-studio.global.extraEnvironmentVars.LABEL_STUDIO_HOST": "{domain}/label-studio",
-    "ingress.host": "{domain}",
-}
-
 
 class LabelStudioHelmStage(NebariHelmStage):
     name = "my-label-studio"
     base_dependency_charts = [label_studio]
-    set_json_template = set_json_template
+
+    def required_inputs(self, stage_outputs: Dict[str, Dict[str, Any]]) -> Dict[str, str]:
+        try:
+            domain = stage_outputs["04-kubernetes-ingress"]["domain"]
+        except KeyError:
+            raise Exception("04-kubernetes-ingress stage must be run before this stage")
+
+        return {
+            "label-studio.global.extraEnvironmentVars.LABEL_STUDIO_HOST": f"https://{domain}/label-studio",
+            "ingress.host": f"{domain}",
+            "startup_greeting": f"Hello from {self.name}!",
+        }
 
 
 @hookimpl
